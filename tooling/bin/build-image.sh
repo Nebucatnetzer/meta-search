e1d97cde0390e1fdfab9e4678da04d7e57651ef9
Andreas Zweili <andreas@zweili.ch>
2025-07-20 00:34:50 +0200

    Add tooling scripts

---
+++ tooling/bin/build-image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+PATH="$(nix build .#pkgs.skopeo --no-link --print-out-paths)/bin:$PATH"
+echo "$GITHUB_TOKEN" | skopeo login ghcr.io --username "$GITHUB_USERNAME" --password-stdin
+result=$(nix build .#szweili-search-image --no-link --print-out-paths)
+# ${variabe,,} converts a string to lowercase
+skopeo copy --insecure-policy docker-archive://"$result" docker://"ghcr.io/${GITHUB_REPOSITORY,,}/snapbin:latest"
