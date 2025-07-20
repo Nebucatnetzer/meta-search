6ea90c96d4a3d093deb19252bcef5428d6492aa1
Andreas Zweili <andreas@zweili.ch>
2025-07-21 00:08:00 +0200

    First build then login

--- tooling/bin/build-app-image.sh
+++ tooling/bin/build-app-image.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 PATH="$(nix build .#ci-tools --no-link --print-out-paths)/bin:$PATH"
-echo "$REGISTRY_PASS" | skopeo login ghcr.io --username "$REGISTRY_USER" --password-stdin
 result=$(nix build .#packages.aarch64-linux.app-image --no-link --print-out-paths)
 # ${variabe,,} converts a string to lowercase
+echo "$REGISTRY_PASS" | skopeo login ghcr.io --username "$REGISTRY_USER" --password-stdin
 skopeo copy --all --insecure-policy docker-archive://"$result" "docker://${REGISTRY,,}/zweili-search-app:latest"
 
 manifest-tool --username $REGISTRY_USER --password $REGISTRY_PASS push from-args --platforms linux/amd64,linux/arm64 --template "${REGISTRY,,}"/zweili-search-app:latest --target $REGISTRY/zweili-search-app:latest
--- tooling/bin/build-nginx-image.sh
+++ tooling/bin/build-nginx-image.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 PATH="$(nix build .#ci-tools --no-link --print-out-paths)/bin:$PATH"
-echo "$REGISTRY_PASS" | skopeo login ghcr.io --username "$REGISTRY_USER" --password-stdin
 result=$(nix build .#nginx-image --no-link --print-out-paths)
 # ${variabe,,} converts a string to lowercase
+echo "$REGISTRY_PASS" | skopeo login ghcr.io --username "$REGISTRY_USER" --password-stdin
 skopeo copy --insecure-policy docker-archive://"$result" docker://"${REGISTRY,,}/zweili-search-nginx:latest"
