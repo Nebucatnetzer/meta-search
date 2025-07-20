c1c82a868eb7c2f482847a9cdc164eab571500ed
Andreas Zweili <andreas@zweili.ch>
2025-07-20 13:13:55 +0200

    Add minimal search example

--- src/search/views.py
+++ src/search/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "search/index.html")
+    query = request.GET.get("query")
+    results = None
+    if query:
+        results = [
+            {"link": "http://foo", "title": "foo"},
+            {"link": "http://bar", "title": "bar"},
+        ]
+    return render(request, "search/index.html", {"results": results})
