458dd2f3deefdee86c2b231e54287d8e781738eb
Andreas Zweili <andreas@zweili.ch>
2025-07-21 00:10:03 +0200

    Lowercase registry path

--- tooling/bin/build-app-image.sh
+++ tooling/bin/build-app-image.sh
@@ -7,4 +7,4 @@ result=$(nix build .#packages.aarch64-linux.app-image --no-link --print-out-path
 echo "$REGISTRY_PASS" | skopeo login ghcr.io --username "$REGISTRY_USER" --password-stdin
 skopeo copy --all --insecure-policy docker-archive://"$result" "docker://${REGISTRY,,}/zweili-search-app:latest"
 
-manifest-tool --username $REGISTRY_USER --password $REGISTRY_PASS push from-args --platforms linux/amd64,linux/arm64 --template "${REGISTRY,,}"/zweili-search-app:latest --target $REGISTRY/zweili-search-app:latest
+manifest-tool --username $REGISTRY_USER --password $REGISTRY_PASS push from-args --platforms linux/amd64,linux/arm64 --template "${REGISTRY,,}"/zweili-search-app:latest --target "${REGISTRY,,}"/zweili-search-app:latest
