91eed2296adc00df05eae4d5989ad9b99c3aca23
Andreas Zweili <andreas@zweili.ch>
2025-07-20 12:11:02 +0200

    Add SearchUser model

--- src/zweili_search/settings.py
+++ src/zweili_search/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "search.apps.SearchConfig",
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -98,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+AUTH_USER_MODEL = "search.SearchUser"
 
 
 # Internationalization
