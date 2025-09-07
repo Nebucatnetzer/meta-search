7321bbf92b5b31e6b8e2939a4eccc7d11c7257ab
Andreas Zweili <andreas@zweili.ch>
2025-09-07 10:47:38 +0200

    Extend build script for nginx

--- tooling/bin/build-nginx-image.sh
+++ tooling/bin/build-nginx-image.sh
@@ -7,3 +7,5 @@ result=$(nix build .#nginx-image --no-link --print-out-paths)
 # ${variabe,,} converts a string to lowercase
 echo "$REGISTRY_PASS" | skopeo login ghcr.io --username "$REGISTRY_USER" --password-stdin
 skopeo copy --insecure-policy docker-archive://"$result" docker://"${REGISTRY,,}/zweili-search-nginx:latest"
+
+manifest-tool --username $REGISTRY_USER --password $REGISTRY_PASS push from-args --platforms linux/arm64 --template "${REGISTRY,,}"/zweili-search-nginx:latest --target "${REGISTRY,,}"/zweili-search-app:latest
