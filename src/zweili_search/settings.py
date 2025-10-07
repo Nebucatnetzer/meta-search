d88454a15604dd748b96fbfe640f8ff64dda185b
Andreas Zweili <andreas@zweili.ch>
2025-10-07 20:59:35 +0200

    Ignore dev key

--- src/zweili_search/settings.py
+++ src/zweili_search/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o7g7m2pl*_opz6@)(w2jb5=@44rsl(e63-lr3m$!u#s$)pym=1"
+SECRET_KEY = (
+    "django-insecure-o7g7m2pl*_opz6@)(w2jb5=@44rsl(e63-lr3m$!u#s$)pym=1"  # noqa: S105
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", None)
@@ -107,7 +109,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "UserAttributeSimilarityValidator"
+        ),
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
